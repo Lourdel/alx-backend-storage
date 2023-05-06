3900d8bda129ae815509911b1e8a1c3a7a12b13c
lourdel <klourdel@gmail.com>
2023-05-06 16:18:41 +0300

    fixing variable error

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -5,7 +5,7 @@ DELIMITER $$
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
-    UPDATE users SET avg_score = (SELECT SUM(projects.weight * corrections.score) / SUM(projects.weight)
+    UPDATE users SET average_score = (SELECT SUM(projects.weight * corrections.score) / SUM(projects.weight)
     FROM projects JOIN corrections ON projects.id = corrections.project_id
     WHERE corrections.user_id = user_id)
     WHERE users.id = user_id;
